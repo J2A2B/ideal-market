2f02fa24a0f38987db5dc0e814d8d6e8a9f78f27
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-05-12 09:15:57 +0200

    Okay, là ya beaucoup d'ajout, je fais une PR

--- controller.js
+++ controller.js
@@ -1,10 +1,12 @@
-
+'use strict'
 app.config(['$routeProvider',function($routeProvider){
 
   $routeProvider.when('/',{
     templateUrl:'search.html'
   }).when('/produit',{
-    templateUrl:'produit.html' 
+    templateUrl:'produit.html'
+  }).when('/recipe',{
+    templateUrl:'recipe.html'
   }).otherwise({
     redirectTo:'/'
   });
@@ -22,7 +24,7 @@ app.controller('MainCtrl',['$scope','$http','$location',function($scope, $http,$
   var monCodeProduit = codeProduit;
 
       $http.get('http://fr.openfoodfacts.org/api/v0/produit/'+monCodeProduit+'.json').then(function(response){
-              
+
               //affichage du nom du produit
               //console.log('Name: '+response.data.product.product_name);
               //affichage de la liste de la desc.
@@ -32,7 +34,7 @@ app.controller('MainCtrl',['$scope','$http','$location',function($scope, $http,$
 
               //parcour de la description si on trouve des recette avec l'aliment selectionné.
               for(var aliment in descriptionAliment){
-                //affichage de la list de la descritpion aliment par aliment . 
+                //affichage de la list de la descritpion aliment par aliment .
                 //console.log(descriptionAliment[aliment]);
                 if (['saumon','poulet','riz'].indexOf(descriptionAliment[aliment]) >-1){
                     monProduit = descriptionAliment[aliment];
@@ -40,19 +42,19 @@ app.controller('MainCtrl',['$scope','$http','$location',function($scope, $http,$
                 }
               }
 
-              //affichage de mon produit . 
+              //affichage de mon produit .
               console.log(monProduit);
               $scope.produit = response.data;
                 console.log($scope.produit)
                 //http pour parcontre le json avec les menus
                   $http.get('recettes.json').then(function(responseJson){
                       //liste toutes recettes
-                      var listeDesRecettes = responseJson.data[0].recettes;
+                      var listeDesRecettes = responseJson.data.recettes;
                       //liste recette choisi
                       var recettesChoisi=[]
                       //affichage de la premiere recette
                       //console.log(listeDesRecettes);
-                      
+
                       for (var recette in listeDesRecettes)
                       {
                         if(monProduit == listeDesRecettes[recette].aliment)
@@ -67,12 +69,34 @@ app.controller('MainCtrl',['$scope','$http','$location',function($scope, $http,$
 
 
       });//fin http pour l'API openfoodfacts
-             
-       
+
+
   };//fin de la fonction de rechercherProduit sur click
+//Début de la récupération des recettes :
+// $http.get('./recettes.json').then(function(response){
+//   $scope.recettes = response;
+// })
+$http.get('recettes.json')
+.then(function successCallback(responseDeux) {
+     $scope.recipe = responseDeux.data.recettes;
+ },
+ function errorCallback(response) {
+     alert(response);
+ });
+
+ //YATAAAA ENFIN REUSSI !!
+ // cette fonction récupère l'objet sur laquelle elle est appeléé
+
+$scope.goToRecipe = function(recetteDeFin){
+  console.log(recetteDeFin.nom);
+  $scope.imgRecette = recetteDeFin.image;
+  $scope.nomRecette = recetteDeFin.nom;
+  //Initialisation du tableau qui récupère le tableau compris dans l'objet
+  $scope.recetteaAfficher = [];
+  $scope.recetteaAfficher.push(recetteDeFin.ingredients);
 
 
-}]); //fin controller
+  $location.path('/recipe')
 
-
-
+}
+ }]); //fin controller
